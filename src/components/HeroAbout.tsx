debc5ffc8e1b6f4a0db1a00373e52a49489ba0a2
BrunoAngeloti <bruno.angeloti@terra.com.br>
2023-11-28 19:13:45 -0300

    feat: about page done

--- src/components/HeroAbout.tsx
+++ src/components/HeroAbout.tsx
@@ -4,33 +4,29 @@ import { useState } from "react";
 import { LoginModal } from "./modals/LoginModal";
 import { SignUpModal } from "./modals/SignUpModal";
 import { SignUpFormModal } from "./modals/SignUpFormModal";
-import { useRouter } from "next/router";
 
-export function Hero(){
+export function HeroAbout(){
   const [isLoginModalOpen, setLoginModalOpen] = useState(false);
   const [isSignUpModalOpen, setSignUpModalOpen] = useState(false);
   const [isSignUpFormModalOpen, setSignUpFormModalOpen] = useState(false);
 
-  const route = useRouter();
-
   return(
     <div className="w-full bg-primary-50 flex items-center flex-col lg:flex-row pt-6 lg:pt-12 gap-6 font-poppins rounded-md z-10">
       <div className="px-6 lg:px-0 lg:pl-14 pb-12 flex flex-col justify-center items-center lg:items-start text-center lg:text-left">
-        <h1 className="text-2xl sm:text-4xl font-semibold mb-1 text-grey-500">COMPARTILHE <span className="text-primary-500">HISTÓRIAS</span></h1>
-        <h1 className="text-2xl sm:text-4xl font-semibold text-grey-500"><span className="text-primary-500">INSPIRE</span> MUDANÇAS</h1>
+        <h1 className="text-2xl sm:text-4xl font-semibold mb-1 text-grey-500">SOBRE A PLATAFORMA</h1>
 
         <p className="text-grey-500 mt-2">
-          Ética e moral no universo da computação. Descubra, discuta e desenvolva perspectivas sobre a tecnologia responsável.
+          Nossa plataforma explora a interseção entre ética e computação, reunindo uma comunidade para compartilhar, debater e aprender. Oferecemos discussões enriquecedoras, recursos educativos e um simulador de dilemas éticos, visando inspirar reflexão e crescimento no mundo digital.
         </p>
 
-        <div className="flex flex-col md:flex-row gap-4 mt-6 w-full justify-center lg:justify-start">
-          <Button title="Saiba mais" onPress={() => route.push("/sobre")}/>
-          <Button title="Criar uma conta" onPress={() => setSignUpModalOpen(true)} variant="outlined"/>
+        <div className="flex flex-col md:flex-row gap-4 mt-6 w-full justify-center lg:justify-start">     
+          <Button title="Criar uma conta" onPress={() => setSignUpModalOpen(true)} />
+          <Button title="Entrar" onPress={() => setLoginModalOpen(true)} variant="outlined" />
         </div>
       </div>
       <Image
-        src="/heroImage.svg"
-        alt="3 pessoas sentadas em uma mesa trabalhando"
+        src="/heroAboutImage.svg"
+        alt="4 pessoas de etinias diferentes"
         width={470}
         height={300}
       />
---
+++ src/pages/sobre.tsx
@@ -0,0 +1,46 @@
+import { HeroAbout } from "@/components/HeroAbout";
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <main className="w-full flex flex-col items-center">
+      <section className="w-full max-w-7xl px-10 font-poppins text-justify">
+        <HeroAbout />
+        <div className="flex flex-col-reverse items-center lg:flex-row gap-8 mt-10">
+          <Image  
+            src="/ufes.png"
+            alt="Logo da UFES"
+            width={490}
+            height={213}
+            className="rounded-lg"
+          />
+
+          <p className="text-grey-500 font-medium">
+            Nosso espaço digital foi pensado para ser um fórum dedicado à reflexão e compartilhamento de ideias sobre temas essenciais que encontram na interseção entre a ética, moralidade e computação. Este blog é uma iniciativa conjunta de professores e alunos da Universidade Federal do Espírito Santo (Ufes), criando um ambiente colaborativo que promove o diálogo e o enriquecimento mútuo.
+            Através deste projeto, buscamos abordar questões contemporâneas que desafiam os paradigmas tradicionais, incentivando uma análise crítica sobre como a computação pode ser alinhada com valores éticos e morais. Ao reunir diferentes perspectivas e experiências, tanto acadêmicas quanto práticas, nossa comunidade se esforça para desenvolver um entendimento mais amplo sobre o papel da tecnologia no mundo moderno. Nossa missão é não apenas educar, mas também inspirar uma nova geração de pensadores e profissionais que valorizem a importância da responsabilidade ética na era digital.
+          </p>
+        </div>
+
+        <p className="text-grey-500 font-medium mt-20">
+          Na era digital em que vivemos, a interação entre a tecnologia e as questões éticas tornou-se fundamental. Nossa comunidade acadêmica está comprometida em explorar as complexidades que surgem quando a inovação tecnológica se encontra com considerações morais e sociais. Aqui, você encontrará reflexões e análises elaboradas por membros da Ufes, abordando tópicos que variam desde os desafios éticos na inteligência artificial até questões cruciais de segurança cibernética.
+        </p>
+
+        <div className="flex flex-col-reverse items-center lg:flex-row gap-8 mt-20">
+          <p className="text-grey-500 font-medium">
+            Este blog é um espaço colaborativo voltado para a comunidade da UFES, tanto profissionais quanto estudantes. Incentivamos a partilha de ideias e experiências sobre ética na computação, segurança cibernética e dilemas morais na era digital. Aqui, você pode expressar seus insights, aprender com os demais e contribuir para um rico diálogo. Este é o lugar ideal para quem é apaixonado por tecnologia e ética. Convidamos você a juntar-se a nós nesta emocionante jornada de conhecimento e descoberta, onde exploramos juntos os desafios e oportunidades do mundo digital, sempre com um olhar crítico e reflexivo. Este é o lugar ideal para quem busca não apenas compreender, mas também moldar o futuro da tecnologia de maneira consciente e ética.
+          </p>
+          <Image  
+            src="/aboutus.svg"
+            alt="Mulher sentada mexendo no computador e um monitor com um blog aberto"
+            width={536}
+            height={266}
+          />         
+        </div>
+
+        <p className="text-grey-500 font-medium mt-10 mb-20">
+          Junte-se a nós enquanto exploramos as interconexões fascinantes entre a tecnologia e a ética, dando voz a diversas perspectivas e contribuindo para um entendimento mais profundo das questões que moldam o nosso mundo digital.
+        </p>
+      </section>
+    </main>
+  )
+}
